96381697ed6d057cad95c80db4aed4acade63d19
楊奕儒 <98101443+ben25000233@users.noreply.github.com>
2025-01-25 15:07:46 +0800

    Update pointnet2_ssg_sem.py

--- dynamics/multimodal/multimodal/Pointnet2_PyTorch/pointnet2/pointnet_models/pointnet2_ssg_sem.py
+++ dynamics/multimodal/multimodal/Pointnet2_PyTorch/pointnet2/pointnet_models/pointnet2_ssg_sem.py
@@ -1,11 +1,11 @@
 import pytorch_lightning as pl
 import torch
 import torch.nn as nn
-from pointnet2_ops.pointnet2_modules import PointnetFPModule, PointnetSAModule
+from Pointnet2_PyTorch.pointnet2_ops_lib.pointnet2_ops.pointnet2_modules import PointnetFPModule, PointnetSAModule
 from torch.utils.data import DataLoader
 
-from pointnet2.data import Indoor3DSemSeg
-from pointnet2.models.pointnet2_ssg_cls import PointNet2ClassificationSSG
+from Pointnet2_PyTorch.pointnet2.data import Indoor3DSemSeg
+from Pointnet2_PyTorch.pointnet2.models.pointnet2_ssg_cls import PointNet2ClassificationSSG
 
 
 class PointNet2SemSegSSG(PointNet2ClassificationSSG):
@@ -48,19 +48,33 @@ class PointNet2SemSegSSG(PointNet2ClassificationSSG):
             )
         )
 
-        # self.FP_modules = nn.ModuleList()
-        # self.FP_modules.append(PointnetFPModule(mlp=[128 , 128, 128, 128]))
-        # self.FP_modules.append(PointnetFPModule(mlp=[256 + 64, 256, 128]))
-        # self.FP_modules.append(PointnetFPModule(mlp=[256 + 128, 256, 256]))
-        # self.FP_modules.append(PointnetFPModule(mlp=[512 + 256, 256, 256]))
-
         self.fc_layer = nn.Sequential(
-            nn.Conv1d(512, 256, kernel_size=1, bias=False),
-            nn.BatchNorm1d(256),
+            nn.Conv1d(512, 128, kernel_size=1, bias=False),
+            nn.BatchNorm1d(128),
             nn.ReLU(True),
-            # nn.Dropout(0.1),
-            nn.Conv1d(256, 16, kernel_size=1),
+            nn.Dropout(0.5),
+            nn.Conv1d(128, 16, kernel_size=1),
         )
+
+        '''
+        # for decoder : 
+        
+        self.FP_modules = nn.ModuleList()
+        self.FP_modules.append(PointnetFPModule(mlp=[128 + self.hparams["feature_num"], 128, 128, 128]))
+        self.FP_modules.append(PointnetFPModule(mlp=[256 + 64, 256, 128]))
+        self.FP_modules.append(PointnetFPModule(mlp=[256 + 128, 256, 256]))
+        self.FP_modules.append(PointnetFPModule(mlp=[512 + 256, 256, 256]))
+
+        
+        self.fc_layer = nn.Sequential(
+            nn.Conv1d(128, 128, kernel_size=1, bias=False),
+            nn.BatchNorm1d(128),
+            nn.ReLU(True),
+            nn.Dropout(0.5),
+            nn.Conv1d(128, 13, kernel_size=1),
+        )
+        '''
+
 
     def forward(self, pointcloud):
         r"""
@@ -74,25 +88,38 @@ class PointNet2SemSegSSG(PointNet2ClassificationSSG):
                 Each point in the point-cloud MUST
                 be formated as (x, y, z, features...)
         """
-   
+  
         xyz, features = self._break_up_pc(pointcloud)
-        for module in self.SA_modules:
-            xyz, features = module(xyz, features)
-        
-        #return features
-        return self.fc_layer(features)
-        
-        l_xyz, l_features = [xyz], [features]
+
+        # for model in self.SA_modules:
+        #     xyz, features = model(xyz, features)
+
+        # print(self.fc_layer(features).shape)
+        # return self.fc_layer(features)
     
+
+    
+        l_xyz, l_features = [xyz], [features]
         for i in range(len(self.SA_modules)):
             li_xyz, li_features = self.SA_modules[i](l_xyz[i], l_features[i])
             l_xyz.append(li_xyz)
             l_features.append(li_features)
-       
+            
+        
+        return self.fc_layer(l_features[-1])
+
+
+        # for decode 
+        
+        # l_xyz, l_features = [xyz], [features]
+        # for i in range(len(self.SA_modules)):
+        #     li_xyz, li_features = self.SA_modules[i](l_xyz[i], l_features[i])
+        #     l_xyz.append(li_xyz)
+        #     l_features.append(li_features)
+
         # for i in range(-1, -(len(self.FP_modules) + 1), -1):
         #     l_features[i - 1] = self.FP_modules[i](
         #         l_xyz[i - 1], l_xyz[i], l_features[i - 1], l_features[i]
         #     )
-        #return self.fc_lyaer(l_features[0])
-        return l_features[-1]
 
+        # return self.fc_layer(l_features[0])
